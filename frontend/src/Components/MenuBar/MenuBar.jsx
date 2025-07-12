55a7896ac412a9619c718bf598c9fba2b449893b
LittleMozart <hoangmp24@gmail.com>
2025-07-12 22:45:23 +0700

    back to cua hang dien tu

--- frontend/src/Components/MenuBar/MenuBar.jsx
+++ frontend/src/Components/MenuBar/MenuBar.jsx
@@ -11,13 +11,13 @@ function MenuBar() {
     <>
       <div className='menu-bar'>
         <div className="label-menu-bar">
-          <Link to='/Body'>
+          <Link to='/DienThoai'>
             <div className="label-item">
               <div className="item-content">
                 <div className="category-icon">
                   <FaCamera style={{fontSize:'27px'}}/>
                 </div>
-                <span className='item-link'>Body</span>
+                <span className='item-link'>Dien Thoai</span>
               </div>
               <div className='right-icon'>
                 <FontAwesomeIcon icon={faChevronRight} />
@@ -26,13 +26,13 @@ function MenuBar() {
           </Link>
         </div>
         <div className="label-menu-bar">
-          <Link to='/Lens'>
+          <Link to='/Laptop'>
             <div className="label-item">
               <div className="item-content">
                 <div className="category-icon">
                   <RiCameraLensFill style={{fontSize:'27px'}}/>
                 </div>
-                <span className='item-link'>Lens</span>
+                <span className='item-link'>Laptop</span>
               </div>
               <div className='right-icon'>
                 <FontAwesomeIcon icon={faChevronRight} />
@@ -41,14 +41,74 @@ function MenuBar() {
           </Link>
         </div>
         <div className="label-menu-bar">
-          <Link to='/Phụ kiện'>
+          <Link to='/TaiNghe'>
             <div className="label-item">
               <div className="item-content">
                 <div className="category-icon">
                 <HiAdjustmentsHorizontal style={{fontSize:'27px'}} />
                 </div>
-                <span style={{textDecoration:'none'}} className='item-link'>Phụ Kiện</span>
+                <span style={{textDecoration:'none'}} className='item-link'>Tai nghe</span>
               </div> 
+              <div className='right-icon'>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="label-menu-bar">
+          <Link to='/BanPhim'>
+            <div className="label-item">
+              <div className="item-content">
+                <div className="category-icon">
+                  <FaCamera style={{fontSize:'27px'}}/>
+                </div>
+                <span className='item-link'>Ban phim</span>
+              </div>
+              <div className='right-icon'>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="label-menu-bar">
+          <Link to='/PhuKien'>
+            <div className="label-item">
+              <div className="item-content">
+                <div className="category-icon">
+                  <FaCamera style={{fontSize:'27px'}}/>
+                </div>
+                <span className='item-link'>Phu Kien</span>
+              </div>
+              <div className='right-icon'>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="label-menu-bar">
+          <Link to='/Chuot'>
+            <div className="label-item">
+              <div className="item-content">
+                <div className="category-icon">
+                  <FaCamera style={{fontSize:'27px'}}/>
+                </div>
+                <span className='item-link'>Chuot</span>
+              </div>
+              <div className='right-icon'>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div className="label-menu-bar">
+          <Link to='/Tivi'>
+            <div className="label-item">
+              <div className="item-content">
+                <div className="category-icon">
+                  <FaCamera style={{fontSize:'27px'}}/>
+                </div>
+                <span className='item-link'>Tivi</span>
+              </div>
               <div className='right-icon'>
                 <FontAwesomeIcon icon={faChevronRight} />
               </div>
